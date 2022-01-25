9653e1e911d6847873dc8d8b358c68adde606d98
Izabela88 <izabkru88@gmail.com>
2022-01-25 14:39:28 +0000

    Commit

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -17,16 +17,12 @@ let openSidebar = true;
 toggleHamburgerIcon();
 
 function handleSidebar() {
-  console.log(openSidebar);
-
   if (openSidebar) {
     openMenu();
     openSidebar = false;
-    console.log('open');
   } else {
     closeMenu();
     openSidebar = true;
-    console.log('close');
   }
 }
 
@@ -43,10 +39,9 @@ hamburgerIcon.addEventListener('click', function (e) {
 });
 
 function toggleWrapperMenuOnResize() {
-  if (window.innerWidth >= 1050) {
+  if (window.innerWidth >= 1150) {
     closeMenu();
     openSidebar = true;
-    console.log('close');
     let wrapperMenu = document.querySelector('.wrapper-menu.open');
     if (wrapperMenu) {
       wrapperMenu.classList.toggle('open');
@@ -61,9 +56,53 @@ window.addEventListener('resize', toggleWrapperMenuOnResize);
 for (const button of closeButtons) {
   let wrapperMenu = document.querySelector('.wrapper-menu');
   button.addEventListener('click', function (e) {
-    if (window.innerWidth < 1050) {
+    if (window.innerWidth < 1150) {
       wrapperMenu.classList.toggle('open');
       handleSidebar();
     }
   });
 }
+
+
+// Change navbar background color on scrolling
+window.addEventListener('scroll', () => {
+  let header = document.querySelector('header');
+
+  if (window.pageYOffset > 100 && window.innerWidth > 1150) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+// Hide and show navbar on scrolling
+
+// Variable which stores position of page
+let previousPosition;
+window.addEventListener('scroll', () => {
+  let header = document.querySelector('header');
+  // Get the scroll position
+  let currentPosition =
+    window.pageYOffset || document.documentElement.currentPosition;
+  // Check weather the page is scrolled up or down
+  if (currentPosition > previousPosition && window.innerWidth > 1150) {
+    header.style.top = '-200px';
+  } else {
+    header.style.top = '0';
+  }
+  previousPosition = currentPosition;
+});
+
+// Change active menu item (link) on page scroll
+window.addEventListener('scroll', (e) => {
+  let sections = document.querySelectorAll('.section');
+  let navLinks = document.querySelectorAll('.nav-link');
+  for (let [i, section] of sections.entries()) {
+    let box = section.getBoundingClientRect();
+    if (box.top <= 10 && box.bottom >= 20) {
+      navLinks[i].classList.add('active');
+    } else {
+      navLinks[i].classList.remove('active');
+    }
+  }
+});
